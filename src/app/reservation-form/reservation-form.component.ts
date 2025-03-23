b0821cf1e7b4dcea8f4c4c7ae0280903afff2c85
Harrison Gough <hgough1@wgu.edu>
2025-03-22 22:31:12 -0500

    input and form controll

--- src/app/reservation-form/reservation-form.component.ts
+++ src/app/reservation-form/reservation-form.component.ts
@@ -9,4 +9,8 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class ReservationFormComponent {
   reservationForm: FormGroup = new FormGroup({});
+
+  onSubmit() {
+
+}
 }
